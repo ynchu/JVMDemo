bf776b9ac28100ef3bc2012d47bb81ea46d8cb0b
ynchu <3319034446@qq.com>
2020-07-05 22:11:48 +0800

    添加javap的demo

---
+++ chapter01/src/org/StackStruTest.java
@@ -0,0 +1,18 @@
+package org;
+
+public class StackStruTest {
+    public static void main(String[] args) {
+//        int i = 2 + 3;
+        int i = 2;
+        int j = 3;
+        int k = i + j;
+
+        try {
+            Thread.sleep(6000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("hello");
+    }
+}
