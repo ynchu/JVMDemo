e2a7d005f98c69cca987aff593c6db1f23ce150b
ynchu <3319034446@qq.com>
2020-07-06 12:27:30 +0800

    添加字节码简单分析

---
+++ chapter02/HELP.md
@@ -0,0 +1,9 @@
+# 帮助
+
+**需要下载插件<code>jclasslib bytecode viewer</code>**
+1. 编译源码
+2. 选择class文件
+3. 点击View中的Show Bytecode With Jclasslib
+
+
+
---
+++ chapter02/src/org/ClinitTest1.java
@@ -0,0 +1,19 @@
+package org;
+
+public class ClinitTest1 {
+    static class Father {
+        public static int A = 1;
+
+        static {
+            A = 2;
+        }
+    }
+
+    static class Son extends Father {
+        public static int B = A;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Son.B);
+    }
+}
